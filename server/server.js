dab62ecc98a4b4ef400b66293ebd359a1dd6f7ab
yessine-ghezaiel <yessineghezaiel@gmail.com>
2021-09-11 00:22:24 +0100

    new3

--- server/server.js
+++ server/server.js
@@ -8,11 +8,6 @@ const cors = require('cors')
 // const PORT = config.get('SERVER_CONFIG.PORT') || 5000
 const PORT = process.env.PORT || 5000
 dbConnect()
-mongoose.connect(process.env.MONGODB_URI , {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify:false,useCreateIndex:true
-  })
 app.use(cors())
 app.use(express.json({ limit: '50MB'}))
 app.use('/api/user',require('./routes/userRoutes'))
